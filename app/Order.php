de7b82843d5cdc1a7be8582ee78e70de66b50346
Popsana Barida <popsyjunior@gmail.com>
2020-06-28 00:16:07 +0100

    set order relationships

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function getQuantityAttribute()
+    {
+        return $this->items()->sum('quantity');
+    }
+
+    public function getTotalCost()
+    {
+        return $this->items->reduce(function ($carriedCost, OrderItem $item) {
+            return $carriedCost + $item->getCost();
+        }, 0.00);
+    }
 }
